a34065f7db9d37b17de87b2e2c5eb9c70121974a
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-03-03 10:57:36 -0300

    Building an application with gulp

---
+++ gulpTasks/servidor.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+const watch = require('gulp-watch');
+
+function monitorarArquivos(cb) {
+  watch('src/paginas/inicio.html', () => {
+    gulp.series('appHTML')();
+  });
+
+  return cb();
+}
+
+function servidor() {
+  return gulp.src('build').pipe(
+    webserver({
+      port: 8080,
+      open: true,
+      livereload: true,
+    })
+  );
+}
+
+module.exports = {
+  monitorarArquivos,
+  servidor,
+};
